b50c9eb90b0f8df470cb49ec07f9703675045901
root <nazrin.pashayeva.std@bhos.edu.az>
2025-06-18 05:36:58 +0000

    commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    count = len(sys.argv)
+    count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
     else:
         print("{} argument{}:".format(count, "" if count == 1 else "s"))
-        for i in range(1, count):
+        for i in range(1, count + 1):
             print("{}: {}".format(i, sys.argv[i]))
