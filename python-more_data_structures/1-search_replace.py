b2cc934f2900c66522d0b88831bcbf401c89aae7
root <nazrin.pashayeva.std@bhos.edu.az>
2025-06-19 08:54:33 +0000

    commit

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = []
     for i in range(len(my_list)):
         if my_list[i] == search:
-            new_list[i] = replace
+            new_list.append(replace)
         else:
-            new_list[i] = my_list[i]
+            new_list.append(my_list[i])
     return new_list
