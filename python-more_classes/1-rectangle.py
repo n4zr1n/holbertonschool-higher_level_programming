2aa9eb84cfb4c6110a72afc5eb8335cd2d651d73
Nazrin <nazrin.pashayeva.std@bhos.edu.az>
2025-06-25 11:36:17 +0000

    commit

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class with area and perimeter methods.
+"""
+
+class Rectangle:
+    """
+    A class that defines a rectangle by its width and height.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
+    
+    def __init__(self, width=0, height=0):
+        """Initialize the rectangle with width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Gets the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height with validation."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
