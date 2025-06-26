a51530e187b01a8a22cabda38e8b6b0fbe72e87f
root <nazrin.pashayeva.std@bhos.edu.az>
2025-06-26 13:10:46 +0000

    commit

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 """
-This module defines a Rectangle class with comparison capabilities.
+This module defines a Rectangle class with
+width, height, area, perimeter, and comparison.
 """
 
 
 class Rectangle:
-    """
-    Defines a rectangle with width, height, and methods to compute area, perimeter,
-    compare rectangles, and represent them as strings.
-    """
+    """Defines a rectangle using width and height."""
 
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """Initialize rectangle with width and height."""
+        """Initialize rectangle with optional width and height."""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
-        """Retrieve the width."""
+        """Get the width."""
         return self.__width
 
     @width.setter
@@ -35,7 +33,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Retrieve the height."""
+        """Get the height."""
         return self.__height
 
     @height.setter
@@ -58,24 +56,30 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return a string representation using print_symbol."""
+        """Return the rectangle as a string with print_symbol."""
         if self.__width == 0 or self.__height == 0:
             return ""
         symbol = str(self.print_symbol)
         return "\n".join([symbol * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
-        """Return a string that can recreate the instance using eval()."""
-        return f"Rectangle({self.__width}, {self.__height})"
+        """
+        Return a string that can recreate the rectangle
+        using eval().
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
-        """Print a message when instance is deleted and decrease count."""
+        """Print message and decrease instance count on deletion."""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """Return the rectangle with the bigger area (or rect_1 if equal)."""
+        """
+        Return the bigger rectangle based on area.
+        If equal, return rect_1.
+        """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
