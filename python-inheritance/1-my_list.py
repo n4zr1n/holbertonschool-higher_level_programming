c12a16ae72a41700359d85d8b9f92b6d045458e6
root <nazrin.pashayeva.std@bhos.edu.az>
2025-06-27 12:12:59 +0000

    commit

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,11 +6,21 @@ This module prints the list in sorted order.
 
 class MyList(list):
     """
-    MyList inherits from built-in list, adds method to print the list sorted.
+    MyList adds a method to print the list sorted.
     """
 
     def print_sorted(self):
         """
         Prints the list in ascending sorted order.
+
+        Example:
+        >>> my_list = MyList()
+        >>> my_list.append(3)
+        >>> my_list.append(1)
+        >>> my_list.append(2)
+        >>> my_list.print_sorted()
+        [1, 2, 3]
+        >>> print(my_list)
+        [3, 1, 2]
         """
         print(sorted(self))
