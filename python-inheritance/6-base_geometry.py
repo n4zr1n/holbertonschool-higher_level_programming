75fde5426d114d10ad4ef6efece16ebc080bdc2c
root <nazrin.pashayeva.std@bhos.edu.az>
2025-06-27 08:35:52 +0000

    commit

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines a BaseGeometry class with public instance method.
+"""
+
+
+class BaseGeometry:
+    """
+    A class defines area.
+    """
+
+    def area(self):
+        """Raises an Exception with the message."""
+        raise Exception("area() is not implemented")
