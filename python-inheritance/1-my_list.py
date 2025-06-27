52e3233b77d4fd3839d8c89e4e02d1e3dfe83bd6
root <nazrin.pashayeva.std@bhos.edu.az>
2025-06-27 12:04:35 +0000

    commit

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
 """
-This module defines MyList class.
-
->>> my_list = MyList([3, 1, 4])
->>> my_list.print_sorted()
-[1, 3, 4]
->>> print(my_list)
-[3, 1, 4]
+This module defines a subclass of list with a method to print the list in sorted order.
 """
 
 
 class MyList(list):
-    """ MyList class inherits from list and includes print_sorted method """
-
-    def __init__(self, *args):
-        """ Instantiates the MyList object """
-        super().__init__(*args)
+    """
+    MyList inherits from built-in list and adds a method to print the list sorted.
+    """
 
     def print_sorted(self):
-        """ Prints the list sorted in ascending order """
+        """
+        Prints the list in ascending sorted order without modifying the original list.
+        """
         print(sorted(self))
-
-    def __str__(self):
-        """ Returns a string representation of the list """
-        return f"[{', '.join(map(str, self))}]"
