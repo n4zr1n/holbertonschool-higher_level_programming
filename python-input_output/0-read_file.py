92b9bee3759a54a306594d350b26f03c20ed631c
root <nazrin.pashayeva.std@bhos.edu.az>
2025-06-28 11:55:26 +0000

    commit

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module creates a function to read and print txt file.
+"""
+
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
