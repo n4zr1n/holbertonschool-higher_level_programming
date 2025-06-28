92239695c69525bf366f4c9c92f320b77e19691e
root <nazrin.pashayeva.std@bhos.edu.az>
2025-06-28 12:38:40 +0000

    commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """
-this module  adds all arguments to a Python list
+This script adds all command-line arguments to a Python list
+and saves them to a file in JSON format.
+
+The file used is `add_item.json`. If it already exists,
+its contents are loaded and extended with new arguments.
+Otherwise, a new list is created.
+
+Functions used:
+- save_to_json_file: saves a Python object to a JSON file
+- load_from_json_file: loads a Python object from a JSON file
 """
 
-
 import sys
-
 from os.path import exists
-
 from save_to_json_file import save_to_json_file
-
 from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
