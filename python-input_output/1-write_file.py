cd5b53200508187b04de30dd08bcf23ff272d4b4
root <nazrin.pashayeva.std@bhos.edu.az>
2025-06-28 11:57:49 +0000

    commit

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,6 @@ This module writes a string to a txt file.
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file and returns number of characters written."""
+    """Writes a string to a text file"""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
