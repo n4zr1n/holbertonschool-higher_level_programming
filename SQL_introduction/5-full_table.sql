3421f297ac05a58193b0a55e2e300712e6699415
root <nazrin.pashayeva.std@bhos.edu.az>
2025-07-01 10:02:50 +0000

    commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,5 @@
 --  a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server
-DESCRIBE first_table;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE()
+  AND TABLE_NAME = 'first_table';
