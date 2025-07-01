f617c29f9ebb47d36541c4cca125f8887b73fa6b
root <nazrin.pashayeva.std@bhos.edu.az>
2025-07-01 06:05:00 +0000

    commit

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,33 @@
+import pickle
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """Serialize the current instance to a file using pickle."""
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
+        except Exception as e:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize an instance from a file using pickle."""
+        try:
+            with open(filename, 'rb') as file:
+                obj = pickle.load(file)
+                if isinstance(obj, cls):
+                    return obj
+                else:
+                    return None
+        except (FileNotFoundError, pickle.PickleError):
+            return None
