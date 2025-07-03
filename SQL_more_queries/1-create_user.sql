3563d0b71dad228df93784f56e3e621a05a10515
root <nazrin.pashayeva.std@bhos.edu.az>
2025-07-03 10:36:06 +0000

    commit

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,11 +1,4 @@
--- Create user if not exists (MySQL 5.7+)
+-- a script that creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- If user existed but password might be outdated, reset password
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges on all databases and tables
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Apply privileges immediately
 FLUSH PRIVILEGES;
