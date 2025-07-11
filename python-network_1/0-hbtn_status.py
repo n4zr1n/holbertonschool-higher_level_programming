ee32222a2d7e8344b2b79a6783b11424a33566cd
root <nazrin.pashayeva.std@bhos.edu.az>
2025-07-11 08:14:43 +0000

    commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
-import urllib.request
+""" Python script that fetches """
+from urllib import request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    req = request.Request("https://intranet.hbtn.io/status ")
+    req.add_header('cfclearance', 'true')
+
+    with request.urlopen(req) as response:
+        body = response.read()
+        utf8_content = body.decode("utf-8")
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(utf8_content))
