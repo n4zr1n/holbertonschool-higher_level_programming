0b7019c962699727258ff28dcb58606aca667367
root <nazrin.pashayeva.std@bhos.edu.az>
2025-07-11 07:27:07 +0000

    commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,8 +6,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        utf8_content = content.decode('utf-8')
         print('Body response:')
-        print('\t- type: {_type}'.format(_type=type(content)))
-        print('\t- content: {_content}'.format(_content=content))
-        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
