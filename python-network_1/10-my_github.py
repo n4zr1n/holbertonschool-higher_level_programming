62b12beeab8f5b5900fe36d34585804ed0b13329
Nazrin Pashayeva <nazrin.pashayeva.std@bhos.edu.az>
2025-07-11 14:29:15 +0400

    Update 10-my_github.py

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -7,6 +7,10 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    url = ""
-    response = requests.post(url, data=data)
-    print(response.text)
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, password))
+    if response.status_code == 200:
+        user_data = response.json()
+        print(user_data.get("id"))
+    else:
+        print("None")
