1c6ff72b4b56f46edcb91b486a9bb1de5dd1c224
root <nazrin.pashayeva.std@bhos.edu.az>
2025-07-11 10:10:53 +0000

    commit

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     q = sys.argv[1] if len(sys.argv) > 1 else ""
     data = {'q': q}
-
     response = requests.post(url, data=data)
+
     try:
         json_data = response.json()
         if json_data:
-            print("f[{}] {}".format(json_data.Id, json_data.__name__))
+            print("f[{}] {}".format(json_data.get('id'), json_data.get('name')))
         else:
             print("No result")
     except ValueError:
