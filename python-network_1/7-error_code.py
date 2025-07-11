fa3be701903e8dbf3efb33fb04770803ece79731
root <nazrin.pashayeva.std@bhos.edu.az>
2025-07-11 09:49:31 +0000

    commit

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -6,8 +6,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    headers = {'cfclearance': 'true'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     if response.status.code >= 400:
         print("Error code: ", response.status.code)
     else:
