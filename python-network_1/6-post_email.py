1c64ec2b13288fbf4ef1bf702d4c434121e99a12
root <nazrin.pashayeva.std@bhos.edu.az>
2025-07-11 09:39:38 +0000

    commit

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = {'email': email}
-    headers = {'cfclearance': 'true'}
-    response = requests.POST(url, headers=headers, data=data)
+    response = requests.POST(url, data=data)
     print(response.headers.get("X-Request-Id"))
